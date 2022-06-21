3e5ac4de97e7613ce505e5366a152a55aba5c6a4
Tudor Teisanu <tudor.teisanu@powerit.dev>
2022-06-21 23:00:11 +0300

    configure new project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/learning'),
+      dir: require('path').join(__dirname, './coverage/royal2'),
       subdir: '.',
       reporters: [
         { type: 'html' },
